d33ccea37ac2f418b0f8551b67b5d295656dae77
Eduardo Godoy <eduardo.godoy@ufms.br>
2023-04-21 18:24:46 -0300

    Adicionando LineChart

--- src/components/GraficoEstrelas/index.jsx
+++ src/components/GraficoEstrelas/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 /**
  * Componente que representa o gráfico de estrelas.
@@ -9,9 +10,11 @@ import PropTypes from 'prop-types';
  *     Vocês podem (e devem) alterar tudo que quiserem, menos a interface
  *     Além disso, usem dos componentes que forem necessários, para isso importem o pacote junto aos "imports".
  */
+
 const agruparPorData = (estrelas, agrupamento) => {
   let agrupamentoPorData = estrelas.reduce((acc, estrela) => {
     const data = new Date(estrela.starred_at);
+
     // Formatar data por agrupamento (dia, semana, mes, ano)
     if (agrupamento === 'dia') {
       data.setHours(0, 0, 0, 0);
@@ -39,16 +42,32 @@ const agruparPorData = (estrelas, agrupamento) => {
   agrupamentoPorData = Object.entries(agrupamentoPorData).sort((a, b) => {
     return new Date(a[0]) - new Date(b[0]);
   });
+  const data = [];
+  agrupamentoPorData.forEach((item) => {
+    data.push({ name: item[0], uv: item[1], pv: item[1], amt: item[1] });
+  });
 
-  return agrupamentoPorData;
+  return data;
 };
 
+const RenderLineChart = (data) => (
+  <LineChart width={730} height={250} data={data}
+    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="name" />
+    <YAxis />
+    <Tooltip />
+    <Legend />
+    <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+  </LineChart>
+);
 
 export function GraficoEstrelas(props) {
   const agrupamentoPorData = agruparPorData(props.estrelas, props.agrupamento);
 
   console.log(agrupamentoPorData);
-  return <div>{'//TODO'}</div>;
+  return <div><RenderLineChart data={agrupamentoPorData}/></div>;
 }
 
 // Definição dos tipos das propriedades recebidas.
